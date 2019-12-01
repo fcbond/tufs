81d3d0610910d1de8dce2fe1d30e2f8b891253e8
Francis Bond <bond@ieee.org>
2019-12-01 12:22:13 +0800

    merged data, notes and program to check stats

---
+++ stats.py
@@ -0,0 +1,42 @@
+##
+## read and give stats for the checked file
+##
+from collections import defaultdict as dd
+
+#links = dd(set)
+links = dict()
+hlinks = dd(set)
+label = dict()
+for l in open("merged.tsv"):
+    r = l.strip().split('\t')
+    if len(r) == 8:
+        label[r[0].strip()] = r[6].strip()[:-7]
+        if r[0].strip() not in links:
+            links[r[0].strip()] = set()
+        if r[5].strip() == 'T' :
+            links[r[0].strip()].add(r[1].strip())
+        elif r[5].strip() == 'H' :
+            hlinks[r[0].strip()].add(r[1].strip())
+        elif r[5].strip() != 'F' :
+            print('What?', l)
+    # else:
+    #     print('Shortie?', l)
+        
+stats = dd(set)
+for t,l in links.items():
+    stats[len(l)].add(t)
+    pos = set(x[-1] for x in l)
+    if len(pos) > 1:
+        print ("POS: ", t, label[t], l)
+    elif t in hlinks:
+        print ("HYPO: ", t, label[t], l)
+    elif(len(l) == 0):
+        print ("ZERO:", label[t], t)
+    
+    #print (t,l)
+
+for n,f in stats.items():
+    sample = list(f)[:3]
+    print(n,len(f),sample,links[sample[0]])
+
+    
